f86ea834e7eec5103f08a19ae559f2227a013970
XyckoCrin <klintfromph@gmail.com>
2022-06-15 00:10:18 +0800

    update

--- Overworld.js
+++ Overworld.js
@@ -6,7 +6,50 @@ class Overworld {
 	}
 	
 	init() {
-		console.log("Hello from the overworld!", this);
+		const image = new Image();
+		image.onload = () => {
+			this.ctx.drawImage(image,0,0)
+		};
+		image.src = "/images/maps/DemoMap.png";
+		
+		
+		const x = 5;
+		const y = 6;
+		
+		const shadow = new Image();
+		shadow.onload = () => {
+			this.ctx.drawImage(shadow,
+			0, //left cut
+			0, //top cut
+			32, //width of cut
+			32, //height of cut
+			x * 16 - 8,
+			y * 16 - 18,
+			32,
+			32
+			
+			)
+		}
+		shadow.src = "/images/people/shadow.png"
+		
+		
+		const hero = new Image();
+		hero.onload = () => {
+			this.ctx.drawImage(hero,
+			0, //left cut
+			0, //top cut
+			32, //width of cut
+			32, //height of cut
+			x * 16 - 8,
+			y * 16 - 18,
+			32,
+			32
+			
+			)
+		};
+		hero.src = "/images/people/hero.png"
+		
+		
 	}
 	
 }
